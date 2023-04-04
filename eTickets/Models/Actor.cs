c8d1860fc035147b9bc7328807ec4d09b732c367
Vahap Onur YILDIRIM <vahaponuryildirim@gmail.com>
2023-04-04 19:35:21 +0300

    Actor Model Added

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Full name is required")]
+        public string? FullName { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string Bio { get; set; }
+    }
+}
